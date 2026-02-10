ea01d0f2755db048d212d6da87c64980b3977635
Noah Magcalas <noah.magcalas@gmail.com>
2026-02-09 21:55:30 -0600

    feat: print game help message when no options are set

--- src/client_package_concept/cli.py
+++ src/client_package_concept/cli.py
@@ -6,17 +6,26 @@ from .launcher import *
 
 
 app = typer.Typer(
+    context_settings={
+        "help_option_names": ["-h", "--help"]
+    },
     pretty_exceptions_enable=False,
     pretty_exceptions_short=False,
+    no_args_is_help=True,
 )
 
 
 @app.command()
 def game(
+    ctx: typer.Context,
     generate: Annotated[bool, typer.Option("-g", "--generate", help="Generate a new map?")] = False,
     run: Annotated[bool, typer.Option("-r", "--run", help="Run a game?")] = False,
     visualize: Annotated[bool, typer.Option("-v", "--visualize", help="Visualize the last game?")] = False,
 ):
+    if not (generate or run or visualize):
+        print(ctx.get_help())
+        return
+
     if generate:
         generate_map()
     if run:
